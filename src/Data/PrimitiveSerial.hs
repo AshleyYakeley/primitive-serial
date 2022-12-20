7c9ea88276d4d92a71dc678e6e424561628e5aae
Ashley Yakeley <ashley@semantic.org>
2022-12-19 21:14:17 -0800

    convert native/little-endian/big-endian

--- src/Data/PrimitiveSerial.hs
+++ src/Data/PrimitiveSerial.hs
@@ -6,7 +6,8 @@ import Data.ByteString
 import Data.ByteString.Internal
 import Data.Word
 import Foreign
-import Prelude hiding (length)
+import Prelude hiding (drop, length)
+import System.Endian
 import System.IO.Unsafe
 
 newtype BSRead a = MkBSRead
@@ -36,6 +37,13 @@ instance Monad BSRead where
 
 instance MonadPlus BSRead
 
+runBSRead :: BSRead a -> StrictByteString -> Maybe (StrictByteString, a)
+runBSRead (MkBSRead f) bs = do
+    (i, a) <- f bs 0
+    if i <= length bs
+        then return (drop i bs, a)
+        else Nothing
+
 runWholeBSRead :: BSRead a -> StrictByteString -> Maybe a
 runWholeBSRead (MkBSRead f) bs = do
     (i, a) <- f bs 0
@@ -49,30 +57,65 @@ bsRead =
         w <- indexMaybe bs i
         return (succ i, w)
 
-data Serialiser a = MkSerialiser
-    { decode :: BSRead a
-    , encode :: a -> StrictByteString
-    }
-
-nativeSerialiser ::
+decodeNative ::
        forall a. Storable a
-    => Serialiser a
-nativeSerialiser = let
+    => BSRead a
+decodeNative = let
     typeSize :: Int
     typeSize = sizeOf (undefined :: a)
-    decode :: BSRead a
-    decode =
-        MkBSRead $ \(BS fptr len) i -> let
-            i' = i + typeSize
-            in if i' <= len
-                   then Just $ (i', unsafePerformIO $ withForeignPtr fptr $ \ptr -> peek $ plusPtr ptr i)
-                   else Nothing
-    encode :: a -> StrictByteString
-    encode a = unsafePerformIO $ create typeSize $ \ptr -> poke (castPtr ptr) a
-    in MkSerialiser {..}
+    in MkBSRead $ \(BS fptr len) i -> let
+           i' = i + typeSize
+           in if i' <= len
+                  then Just $ (i', unsafePerformIO $ withForeignPtr fptr $ \ptr -> peek $ plusPtr ptr i)
+                  else Nothing
+
+encodeNative ::
+       forall a. Storable a
+    => a
+    -> StrictByteString
+encodeNative a = let
+    typeSize :: Int
+    typeSize = sizeOf (undefined :: a)
+    in unsafePerformIO $ create typeSize $ \ptr -> poke (castPtr ptr) a
 
 class Storable a => FixedNumeric a where
-    swapBytes :: a -> a
+    nativeToLittleEndian :: a -> a
+    nativeToBigEndian :: a -> a
 
 instance FixedNumeric Word8 where
-    swapBytes = id
+    nativeToLittleEndian = id
+    nativeToBigEndian = id
+
+instance FixedNumeric Word16 where
+    nativeToLittleEndian = fromLE16
+    nativeToBigEndian = fromBE16
+
+instance FixedNumeric Word32 where
+    nativeToLittleEndian = fromLE32
+    nativeToBigEndian = fromBE32
+
+instance FixedNumeric Word64 where
+    nativeToLittleEndian = fromLE64
+    nativeToBigEndian = fromBE64
+
+decodeLittleEndian ::
+       forall a. FixedNumeric a
+    => BSRead a
+decodeLittleEndian = fmap nativeToLittleEndian $ decodeNative
+
+encodeLittleEndian ::
+       forall a. FixedNumeric a
+    => a
+    -> StrictByteString
+encodeLittleEndian = encodeNative . nativeToLittleEndian
+
+decodeBigEndian ::
+       forall a. FixedNumeric a
+    => BSRead a
+decodeBigEndian = fmap nativeToBigEndian $ decodeNative
+
+encodeBigEndian ::
+       forall a. FixedNumeric a
+    => a
+    -> StrictByteString
+encodeBigEndian = encodeNative . nativeToBigEndian

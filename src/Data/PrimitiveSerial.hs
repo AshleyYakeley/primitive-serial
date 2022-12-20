758909392ff2a7d8398424344d45cb9040dd04f4
Ashley Yakeley <ashley@semantic.org>
2022-12-19 22:39:42 -0800

    simplify BSRead, more FixedNumeric instances

--- src/Data/PrimitiveSerial.hs
+++ src/Data/PrimitiveSerial.hs
@@ -1,61 +1,74 @@
-module Data.PrimitiveSerial where
+module Data.PrimitiveSerial
+    ( BSRead(..)
+    , runWholeBSRead
+    , bsRead
+    , bsReadN
+    , decodeNative
+    , encodeNative
+    , FixedNumeric(..)
+    , decodeLittleEndian
+    , encodeLittleEndian
+    , decodeBigEndian
+    , encodeBigEndian
+    ) where
 
 import Control.Applicative
 import Control.Monad
 import Data.ByteString
 import Data.ByteString.Internal
+import Data.Int
 import Data.Word
 import Foreign
-import Prelude hiding (drop, length)
+import Prelude hiding (drop, length, null, take)
 import System.Endian
 import System.IO.Unsafe
 
 newtype BSRead a = MkBSRead
-    { unBSRead :: StrictByteString -> Int -> Maybe (Int, a)
+    { runBSRead :: StrictByteString -> Maybe (StrictByteString, a)
     }
 
 instance Functor BSRead where
-    fmap ab (MkBSRead f) = MkBSRead $ \bs i -> fmap (fmap ab) $ f bs i
+    fmap ab (MkBSRead f) = MkBSRead $ \bs -> fmap (fmap ab) $ f bs
 
 instance Applicative BSRead where
-    pure a = MkBSRead $ \_ i -> Just (i, a)
+    pure a = MkBSRead $ \bs -> Just (bs, a)
     MkBSRead fab <*> MkBSRead fa =
-        MkBSRead $ \bs i -> do
-            (i', ab) <- fab bs i
-            (i'', a) <- fa bs i'
-            return (i'', ab a)
+        MkBSRead $ \bs -> do
+            (bs', ab) <- fab bs
+            (bs'', a) <- fa bs'
+            return (bs'', ab a)
 
 instance Alternative BSRead where
-    MkBSRead fa <|> MkBSRead fb = MkBSRead $ \bs i -> fa bs i <|> fb bs i
-    empty = MkBSRead $ \_ _ -> Nothing
+    MkBSRead fa <|> MkBSRead fb = MkBSRead $ \bs -> fa bs <|> fb bs
+    empty = MkBSRead $ \_ -> Nothing
 
 instance Monad BSRead where
     MkBSRead f >>= q =
-        MkBSRead $ \bs i -> do
-            (i', a) <- f bs i
-            unBSRead (q a) bs i'
+        MkBSRead $ \bs -> do
+            (bs', a) <- f bs
+            runBSRead (q a) bs'
 
 instance MonadPlus BSRead
 
-runBSRead :: BSRead a -> StrictByteString -> Maybe (StrictByteString, a)
-runBSRead (MkBSRead f) bs = do
-    (i, a) <- f bs 0
-    if i <= length bs
-        then return (drop i bs, a)
-        else Nothing
-
 runWholeBSRead :: BSRead a -> StrictByteString -> Maybe a
-runWholeBSRead (MkBSRead f) bs = do
-    (i, a) <- f bs 0
-    if i == length bs
+runWholeBSRead ra bs = do
+    (bs', a) <- runBSRead ra bs
+    if null bs'
         then return a
         else Nothing
 
 bsRead :: BSRead Word8
 bsRead =
-    MkBSRead $ \bs i -> do
-        w <- indexMaybe bs i
-        return (succ i, w)
+    MkBSRead $ \bs -> do
+        w <- indexMaybe bs 0
+        return (drop 1 bs, w)
+
+bsReadN :: Int -> BSRead StrictByteString
+bsReadN n =
+    MkBSRead $ \bs -> do
+        if n > length bs
+            then Nothing
+            else return (drop n bs, take n bs)
 
 decodeNative ::
        forall a. Storable a
@@ -63,11 +76,10 @@ decodeNative ::
 decodeNative = let
     typeSize :: Int
     typeSize = sizeOf (undefined :: a)
-    in MkBSRead $ \(BS fptr len) i -> let
-           i' = i + typeSize
-           in if i' <= len
-                  then Just $ (i', unsafePerformIO $ withForeignPtr fptr $ \ptr -> peek $ plusPtr ptr i)
-                  else Nothing
+    in MkBSRead $ \bs@(BS fptr len) ->
+           if typeSize <= len
+               then Just $ (drop typeSize bs, unsafePerformIO $ withForeignPtr fptr $ \ptr -> peek $ castPtr ptr)
+               else Nothing
 
 encodeNative ::
        forall a. Storable a
@@ -78,9 +90,19 @@ encodeNative a = let
     typeSize = sizeOf (undefined :: a)
     in unsafePerformIO $ create typeSize $ \ptr -> poke (castPtr ptr) a
 
+unsafeStorableCoerce ::
+       forall a b. (Storable a, Storable b)
+    => a
+    -> b
+unsafeStorableCoerce a = unsafePerformIO $ with a $ \ptr -> peek $ castPtr ptr
+
 class Storable a => FixedNumeric a where
     nativeToLittleEndian :: a -> a
     nativeToBigEndian :: a -> a
+
+instance FixedNumeric Bool where
+    nativeToLittleEndian = id
+    nativeToBigEndian = id
 
 instance FixedNumeric Word8 where
     nativeToLittleEndian = id
@@ -97,6 +119,22 @@ instance FixedNumeric Word32 where
 instance FixedNumeric Word64 where
     nativeToLittleEndian = fromLE64
     nativeToBigEndian = fromBE64
+
+instance FixedNumeric Int8 where
+    nativeToLittleEndian = id
+    nativeToBigEndian = id
+
+instance FixedNumeric Int16 where
+    nativeToLittleEndian = unsafeStorableCoerce . nativeToLittleEndian @Word16 . unsafeStorableCoerce
+    nativeToBigEndian = unsafeStorableCoerce . nativeToBigEndian @Word16 . unsafeStorableCoerce
+
+instance FixedNumeric Int32 where
+    nativeToLittleEndian = unsafeStorableCoerce . nativeToLittleEndian @Word32 . unsafeStorableCoerce
+    nativeToBigEndian = unsafeStorableCoerce . nativeToBigEndian @Word32 . unsafeStorableCoerce
+
+instance FixedNumeric Int64 where
+    nativeToLittleEndian = unsafeStorableCoerce . nativeToLittleEndian @Word64 . unsafeStorableCoerce
+    nativeToBigEndian = unsafeStorableCoerce . nativeToBigEndian @Word64 . unsafeStorableCoerce
 
 decodeLittleEndian ::
        forall a. FixedNumeric a
